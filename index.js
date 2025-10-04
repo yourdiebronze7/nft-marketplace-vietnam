67ee8b214bfeae2f2d5bdc6de5a03ce821468fb0
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-04 17:00:45 +0000

    Fix formatting of import statement for better readability

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => {
+})
+.then(() => {
   console.log('Successfully connected to the database');
-}).catch(err => {
+})
+.catch(err => {
   console.error('Error connecting to the database:', err);
 });
 
